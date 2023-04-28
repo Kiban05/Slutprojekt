7da19774c202b0998d807a157405f41879cceb84
Kiban05 <kianmolholm@gmail.com>
2023-04-28 09:49:59 +0200

    updaterade lite

--- Luffarshack.py
+++ Luffarshack.py
@@ -1,17 +1,27 @@
 import pygame
 
-WIDTH, HEIGHT = 501, 502
+WIDTH, HEIGHT = 500, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
+FPS = 60 
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+def draw_window():
+    WIN.fill(WHITE)
+pygame.display.update()
+
 def main():
+    clock = pygame.time.Clock()
     run = True
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-
+        draw_window()
+    
     pygame.quit()
 
 if __name__ == "__main__":
     main()
-
