bd184e65774ca620be8ec0bd6f0d9261d9d27390
Kiban05 <kianmolholm@gmail.com>
2023-05-08 22:23:07 +0200

    Lagt till ett rutnät

--- Luffarshack.py
+++ Luffarshack.py
@@ -1,24 +1,38 @@
 import pygame
 
-WIDTH, HEIGHT = 500, 500
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-
+SIZE = 600
+ROWS = 20
+WIN = pygame.display.set_mode((SIZE, SIZE))
+SpaceBtwRows = SIZE // ROWS
 FPS = 60 
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
+def grid(WIN, SIZE, ROWS):
+    x = 0 
+    y = 0
+    for l in range(ROWS):
+        x += SpaceBtwRows
+        y += SpaceBtwRows
+        pygame.draw.line(WIN, (BLACK), (x, 0), (x, SIZE))
+
+        pygame.draw.line(WIN, (BLACK), (0, y), (SIZE, y))
+
 def draw_window():
     WIN.fill(WHITE)
-pygame.display.update()
+    grid(WIN, SIZE, ROWS)
+    pygame.display.update()
 
 def main():
     clock = pygame.time.Clock()
     run = True
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        
         draw_window()
     
     pygame.quit()
