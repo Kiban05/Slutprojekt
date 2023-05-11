36575059fb3b8484edc6523ab2cc04bbe56a9d6e
Kiban05 <kianmolholm@gmail.com>
2023-05-11 23:00:51 +0200

    Kryss och Cirkel

--- Luffarshack.py
+++ Luffarshack.py
@@ -1,10 +1,16 @@
 import pygame
+import numpy as np
+import os
 
 SIZE = 600
 ROWS = 20
 WIN = pygame.display.set_mode((SIZE, SIZE))
 SpaceBtwRows = SIZE // ROWS
 FPS = 60 
+CROSS_IMAGE = pygame.image.load(os.path.join('cross.png'))
+CROSS_IMAGE = pygame.transform.scale(CROSS_IMAGE, (24, 24))
+
+cells = np.zeros((ROWS,ROWS), dtype=int)
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -22,8 +28,17 @@ def grid(WIN, SIZE, ROWS):
 def draw_window():
     WIN.fill(WHITE)
     grid(WIN, SIZE, ROWS)
+    for row in range(ROWS):
+        for column in range(ROWS):
+            if cells[row, column] == 1:
+                x = row * SpaceBtwRows
+                y = column * SpaceBtwRows
+                #pygame.draw.circle(WIN, (BLACK), (x + 15, y + 15), 12)
+                WIN.blit(CROSS_IMAGE, (x + 3, y + 3))
+    
     pygame.display.update()
 
+    
 def main():
     clock = pygame.time.Clock()
     run = True
@@ -32,9 +47,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                row, column = pos[0] // SpaceBtwRows, pos[1] // SpaceBtwRows
+                cells[row, column] = 1
+                
+                        
         draw_window()
-    
+        
+
     pygame.quit()
 
 if __name__ == "__main__":
